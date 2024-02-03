b7f16f4a66461d910a1ee3a216cf91ca9ee63a7e
Arthur Vieira Bruske <avieirabruske@gmail.com>
2024-02-03 04:48:15 -0300

    myPortfolio

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Android Studio</li>
+        <li>phpMyAdmin</li>
+        <li>Xampp</li>
+        <li>Lucidchart</li>
+        <li>ReactNative</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>EM. Padre Valente Simioni (2012/2020)</li>
+        <li>Ensino Médio em Senac Joinville (2021/2023)</li>
+        <li>Faculdade Senac Joinville (2024/2026)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Curso Técnico de Desenvolvimento Web</li> 
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/apple-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">Sobre mim</h2>
+          <p className="text-base lg:text-lg">
+            Estou cursando a Faculdade de Análise e Desenvolvimento de Sistemas (ADS), e estou em busca de uma oportunidade para atuar como desenvolvedor. Nas linguagens de Front-End, tenho experiência em HTML5, CSS3 e JavaScript. Já em Back-End, possuo conhecimento em PHP, MySQL, JavaScript e Bash/Shell (práticas obtidas em meu Ensino Médio Integrado ao Curso Técnico de Técnologia para Internet na Faculdade Senac Joinville).
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Habilidades{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Educação{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certificações{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span></span>
+        <p className="text-white">2021 - 2024 © Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
